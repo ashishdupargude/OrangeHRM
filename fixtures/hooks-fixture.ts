ae472c74c22a98395460372a902bfe2ad39bbe29
Ashishdupargude <dupargudeashish1234@gmail.com>
2026-07-31 16:32:01 +0530

    Replace beforeEach and afterEach hooks with custom Playwright fixtures

---
+++ fixtures/hooks-fixture.ts
@@ -0,0 +1,24 @@
+import {test as baseTest} from './commom-fixture'
+
+type HooksFixureType ={
+
+    gotoUrl : any
+    logout: any
+
+}
+
+export const test = baseTest.extend<HooksFixureType>({
+    gotoUrl: async({loginPage}, use) => {
+
+        await loginPage.gotoOrangeHRM()
+        await use()
+
+    },
+    logout: async({userPage}, use) =>{
+        await use()
+        await  userPage.logout()
+
+
+
+    }
+})
--- tests/temp-test.spec.ts
+++ tests/temp-test.spec.ts
@@ -1,24 +1,31 @@
 //import { test } from '../fixtures/pom-fixture'
 import CommonUtils from '../utils/common-util'
 import { expect } from '@playwright/test'
-import { test } from '../fixtures/commom-fixture'
+import { test } from '../fixtures/hooks-fixture'
 import { LoginPage } from '../pages/LoginPage'
 
 
 
+// test.beforeEach('Before Each Hook', async({loginPage})=>{
+//     await loginPage.gotoOrangeHRM()
+
+// })
 
 
-test('temp', async ({ page, loginPage, commonUtils }) => {
+// test.afterEach('After Each Hook', async ({userPage})=> {
+//     await userPage.logout()
+
+// })
+
+
+test('temp', async ({ page, gotoUrl, loginPage, commonUtils }) => {
 
     // console.log(process.env.BASE_URL)
     // console.log(process.env.USER_NAME)
     // console.log(process.env.PASSWORD)
-
-
-
+    
     // await loginPage.gotoOrangeHRM()
     // await loginPage.loginOrangeHRM('Admin', 'admin123')
-
 
     //const commonUtilsObj = new CommonUtils()
     // commonUtilsObj.encryptData('admin123')
@@ -26,15 +33,22 @@ test('temp', async ({ page, loginPage, commonUtils }) => {
     // const decryptedUserName = commonUtils.decryptData(process.env.USER_NAME!)
     // const decryptedPassword = commonUtils.decryptData(process.env.PASSWORD!)
 
-    await loginPage.gotoOrangeHRM()
+    // await loginPage.gotoOrangeHRM()   //Now We are useing Hooks for login 
     console.log(await page.title())
     //await loginPage.loginOrangeHRM(decryptedUserName, decryptedPassword)
 
     //await page.pause()
 
 })
-test('Temp test 2', async ({page,loginPage})=>{
-    await loginPage.gotoOrangeHRM()
+test('Temp test 2', async ({ page, gotoUrl, loginPage }) => {
+    // await loginPage.gotoOrangeHRM()
     await expect(page).toHaveTitle('OrangeHRM')
 
 })
+
+test('Temp test 3', async ({ page, gotoUrl,logout, loginPage }) => {
+    // await loginPage.gotoOrangeHRM()
+    await expect(page).toHaveTitle('OrangeHRM')
+
+})
+
