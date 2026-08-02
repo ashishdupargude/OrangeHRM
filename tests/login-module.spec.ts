f5ce32dde20faabf666f8672a7694e7b0aef1ec8
Ashishdupargude <dupargudeashish1234@gmail.com>
2026-08-02 19:22:15 +0530

    feat: add test.describe grouping and test.step integration

--- tests/login-module.spec.ts
+++ tests/login-module.spec.ts
@@ -2,14 +2,16 @@ import { test, expect } from '../fixtures/hooks-fixture'
 
 import loginModuleData from '../data/login-module-data.json'
 
-test.use({storageState:{
-    cookies: [],
-    origins: []
-}})
+test.use({
+    storageState: {
+        cookies: [],
+        origins: []
+    }
+})
 
 test('[Login] verify that the user cannot log in with invalid password', {
-    tag:['@UI','@UAT']
-},async ({ gotoUrl, loginPage, commonUtils }) => {
+    tag: ['@UI', '@UAT']
+}, async ({ gotoUrl, loginPage, commonUtils }) => {
 
     const username = commonUtils.decryptData(process.env.USER_NAME!)
 
@@ -17,31 +19,44 @@ test('[Login] verify that the user cannot log in with invalid password', {
     await expect(loginPage.invaildCredentialErrorPopup).toHaveText(loginModuleData.invaild_credentials_text)
     await expect(loginPage.userNameInput).toBeVisible()
 })
-
-test('[Login] verify that the user cannot log in with invalid username',{
-    tag:['@UI','@UAT'],
+test.describe('Invalid Login Test', {
+    tag: '@Invalid',
     annotation: {
-        type: 'Test Case Link',
-       description : 'https://github.com/ashishdupargude/OrangeHRM.git'
+        type: 'Story Link',
+        description: 'link of Story'
+
     }
-}, async ({ gotoUrl, loginPage, commonUtils }) => {
 
-    const coreectpassword = commonUtils.decryptData(process.env.PASSWORD!)
+}, () => {
 
-    await loginPage.loginOrangeHRM(loginModuleData.wrong_username,coreectpassword)
-    await expect(loginPage.invaildCredentialErrorPopup).toHaveText(loginModuleData.invaild_credentials_text)
-    await expect(loginPage.userNameInput).toBeVisible()
+
+
+    test('[Login] verify that the user cannot log in with invalid username', {
+        tag: ['@UI', '@UAT'],
+        annotation: {
+            type: 'Test Case Link',
+            description: 'https://github.com/ashishdupargude/OrangeHRM.git'
+        }
+    }, async ({ gotoUrl, loginPage, commonUtils }) => {
+
+        const coreectpassword = commonUtils.decryptData(process.env.PASSWORD!)
+
+        await loginPage.loginOrangeHRM(loginModuleData.wrong_username, coreectpassword)
+        await expect(loginPage.invaildCredentialErrorPopup).toHaveText(loginModuleData.invaild_credentials_text)
+        await expect(loginPage.userNameInput).toBeVisible()
+    })
+
+
+    test('[Login] verify that the user cannot log in with both invalid username and password', {
+        tag: ['@UI', '@DEV']
+    }, async ({ gotoUrl, loginPage, commonUtils }) => {
+
+
+
+        await loginPage.loginOrangeHRM(loginModuleData.wrong_username, loginModuleData.worng_password)
+        await expect(loginPage.invaildCredentialErrorPopup).toHaveText(loginModuleData.invaild_credentials_text)
+        await expect(loginPage.userNameInput).toBeVisible()
+    })
+
+
 })
-
-
-test('[Login] verify that the user cannot log in with both invalid username and password',{
-    tag:['@UI','@DEV']
-}, async ({ gotoUrl, loginPage, commonUtils }) => {
-
-   
-
-    await loginPage.loginOrangeHRM(loginModuleData.wrong_username,loginModuleData.worng_password)
-    await expect(loginPage.invaildCredentialErrorPopup).toHaveText(loginModuleData.invaild_credentials_text)
-    await expect(loginPage.userNameInput).toBeVisible()
-})
-
--- tests/pim-module.spec.ts
+++ tests/pim-module.spec.ts
@@ -1,15 +1,31 @@
- import{test,expect} from '../fixtures/hooks-fixture'
+import { test, expect } from '../fixtures/hooks-fixture'
 import pimData from '../data/pim-module-data.json'
 
- test('[PIM] Verify that a new employee is successfully created under the PIM module',{
-   tag:['@UI','@UAT']
- }, async({gotoUrl,leftNavigationPage,pimPage})=>{
+test('[PIM] Verify that a new employee is successfully created under the PIM module', {
+  tag: ['@UI', '@UAT'],
+  annotation: {
+    type: 'Test Case Link',
+    description: 'https://github.com/ashishdupargude/OrangeHRM.git'
+  }
+}, async ({ gotoUrl, leftNavigationPage, pimPage }) => {
+
+  await test.step("open PIM Module", async ({ }) => {
 
     await leftNavigationPage.openPimModule()
-    await pimPage.addEmployee(pimData.first_name,pimData.middle_name,pimData.last_name)
-    
+
+  })
+
+   await test.step('Add Employee in PIM Module', async () => {
+
+    await pimPage.addEmployee(pimData.first_name, pimData.middle_name, pimData.last_name)
     await expect(pimPage.newEmployeeNameHeading).toHaveText(`${pimData.first_name} ${pimData.last_name}`)
-    
+
+  })
 
 
- })
+
+
+
+
+
+})
