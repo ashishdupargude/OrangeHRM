10c1df0b8879cb273e6df2f8036ce2b79a09d0ee
Ashishdupargude <dupargudeashish1234@gmail.com>
2026-08-11 17:57:34 +0530

    test: add assertions for booking IDs API

--- tests/api-tests/restful-booker-api-module.spec.ts
+++ tests/api-tests/restful-booker-api-module.spec.ts
@@ -1,5 +1,5 @@
 import { request } from 'node:http'
-import{test} from '../../fixtures/hooks-fixture'
+import{test,expect} from '../../fixtures/hooks-fixture'
 
 test("API Testing", async({request}) => {
 
@@ -13,4 +13,31 @@ test("API Testing", async({request}) => {
 test("API test 2", async({request}) =>{
     const bookingDetails = await request.get("https://restful-booker.herokuapp.com/booking/1")
     console.log(await bookingDetails.json())
+})
+
+test("[Restful- Booking]Verify that the user is able to fetch all the booking IDs using GET API and receive valid response", {
+    tag:['@API','@UAT'],
+    annotation:{
+        type: "Test Case Link",
+        description: "Link Of TestCase"
+    }
+},async({request}) => {
+
+    const bookingIDsResp = await  request.get("https://restful-booker.herokuapp.com/booking")
+    const bookingIDsJsonResp = await bookingIDsResp.json()
+    console.log(bookingIDsJsonResp)
+
+    expect(bookingIDsResp.status()).toBe(200)
+    expect(bookingIDsResp.statusText()).toBe('OK')
+    //expect(bookingIDsResp.ok()).toBeTruthy()
+    expect(bookingIDsResp.headers()['content-type']).toBe('application/json; charset=utf-8')
+    expect(bookingIDsResp).not.toBeNull()
+
+
+
+
+
+
+
+
 })
