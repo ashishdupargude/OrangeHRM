e22925c8c5b0f09c83c5abbdfe69e15f5d6b8331
Ashishdupargude <dupargudeashish1234@gmail.com>
2026-08-13 19:17:55 +0530

    Add POST API test for booking creation

--- tests/api-tests/restful-booker-api-module.spec.ts
+++ tests/api-tests/restful-booker-api-module.spec.ts
@@ -1,6 +1,7 @@
 //import { request } from 'node:http'
 import { test, expect } from '../../fixtures/hooks-fixture'
 import restfulApiData from '../../data/api-data/restful-booker-api-module-data.json'
+import { request } from 'node:http'
 test("API Testing", async ({ request }) => {
 
 
@@ -15,7 +16,7 @@ test("API test 2", async ({ request }) => {
     console.log(await bookingDetails.json())
 })
 
-test("[Restful- Booking]Verify that the user is able to fetch all the booking IDs using GET API and receive valid response", {
+test("ID -8 - [Restful- Booking]Verify that the user is able to fetch all the booking IDs using GET API and receive valid response", {
     tag: ['@API', '@UAT'],
     annotation: {
         type: "Test Case Link",
@@ -34,24 +35,41 @@ test("[Restful- Booking]Verify that the user is able to fetch all the booking ID
     expect(bookingIDsResp).not.toBeNull()
 })
 
-test("[Restful-Booker > Booking] Verify that the user is able to fetch booking is useing GET API and receives", {
+test("ID -9 -[Restful-Booker > Booking] Verify that the user is able to fetch booking is useing GET API and receives", {
     tag: ['@API', '@UAT'],
     annotation: {
         type: 'Test Case Link',
         description: 'TestCasrLink'
     }
 }, async ({ request }) => {
-   const bookingResp = await request.get(`https://restful-booker.herokuapp.com/booking/${restfulApiData.booking_id}`)
-   console.log(bookingResp)
-    const bookingJsonResp =await bookingResp.json()
+    const bookingResp = await request.get(`https://restful-booker.herokuapp.com/booking/${restfulApiData.booking_id}`)
+    console.log(bookingResp)
+    const bookingJsonResp = await bookingResp.json()
     console.log(bookingJsonResp)
     expect(bookingResp.status()).toBe(200)
     expect(bookingResp.statusText()).toBe("OK")
-     expect(bookingResp).not.toBeNull()
-     //expect(bookingJsonResp.firstname).toEqual(restfulApiData.firstname)
-     expect(bookingJsonResp.firstname).toBeTruthy()
+    expect(bookingResp).not.toBeNull()
+    //expect(bookingJsonResp.firstname).toEqual(restfulApiData.firstname)
+    expect(bookingJsonResp.firstname).toBeTruthy()
+})
 
+test("ID -10 -[Restful-Booker > Booking ] Verify that the user is able to create new booking useing Post API and recevie valid response.", {
+    tag: ['@API', '@UAT'],
+    annotation: {
+        type: 'Test Case Link',
+        description: 'TestCasrLink'
+    }
+}, async ({ request }) => {
 
+    const createBookingRest = await request.post('https://restful-booker.herokuapp.com/booking', {
+
+        data: restfulApiData.create_booking
+
+    })
+    const createBookigJsonResp = await createBookingRest.json()
+    console.log(createBookigJsonResp)
+    expect(createBookingRest.status()).toBe(200)
+    expect(createBookigJsonResp.booking).toMatchObject(restfulApiData.create_booking)
 
 
 })
