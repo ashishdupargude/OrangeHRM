80ea444bb38ab1454b13df618aebb3ddc02c6da0
Ashishdupargude <dupargudeashish1234@gmail.com>
2026-08-19 22:29:23 +0530

    test(admin): add new user creation test case for Admin module

---
+++ tests/ui-tests/admin-module.spec.ts
@@ -0,0 +1,17 @@
+// import{test,expect} from '../../fixtures/hooks-fixture'
+
+
+// test('[Admin] Verify that new user is created under Admin module', {
+//     tag:['@UI','@UAT'],
+//     annotation:{
+//         type: 'Test Case Link',
+//         description: 'https://github.com/ashishdupargude/OrangeHRM.git'
+//     }
+// },async({page}) => {
+//     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+//     await page.getByRole('textbox',{name:'Username'}).fill('Admin')
+//     await page.getByRole('textbox',{name:'Password'}).fill('admin123')
+//     await page.getByRole('button',{name:'Login'}).click()
+    
+
+// })
